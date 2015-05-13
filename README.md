1d06e657db2655ea5585a70d7a86f8254ba41c0a
Andrew McClure <andrew@southweb.co.nz>
2015-05-13 22:35:53 +1200

    Initial alpha release

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # mqtt-proxy
+
+This project aims to solve the problem of exposing matching individual clients to specific brokers via a single public facing host/port combination.
+
+Configuration:
+
+Edit map.json to create an array of maps.  Example file below.
+[{"username": "*",  "clientId": "*", "host": "localhost", "port": "1883"}]
+
+Matching:
+1. A simple RegEx is performed between the client supplied parameters and those in the map.
+2. The corresponding host and port are used with existing connection parameters proxied.
+
+This is a relatively green project with debugging enabled; without a set of test scripts; and with no support yet for ssl/tls certificates
+
+Please feel free to fork and improve.
---
+++ mqtt-proxy.js
@@ -0,0 +1,199 @@
+var mqtt = require('mqtt')
+  , util = require('util');
+
+
+function refreshMap(){
+    //Replace with an on HUP and build reg exs
+    return  require(__dirname + '/map.json');
+}
+
+function getMap(packet){
+ var maps = refreshMap();
+ var map;
+
+ test = function(a,b){
+    b = (b === 'undefined') ? "":b;
+    s = new RegExp(a.replace('*','\.*') + '$')
+    return s.test(b);
+ }
+
+ for (i in maps){
+    if (this.test(maps[i].username,packet.username) &&
+        this.test(maps[i].clientId,packet.clientId)){
+        map = i; // find first match
+        break;
+    }
+ }
+ return (map === 'undefined') ? null: maps[map]; 
+}
+
+
+new mqtt.Server(function(client) {
+  var self = this;
+
+  if (!self.proxies) self.proxies = {};
+  if (!self.clients) self.clients = {};
+
+  process.on('SIGINT', function() {
+    console.log('Received interrupt');
+    for (c in self.clients){
+      self.clients[c].emit('close');
+    }
+    process.exit();
+});
+
+
+  
+  client.on('connect', function(packet) {
+    console.log(packet)
+    client.id = packet.clientId;
+    console.log("CONNECT: client id: " + client.id);
+    client.subscriptions = [];
+
+    var options ={}
+    var map = getMap(packet)
+    if (!map){
+        client.connack({returnCode:3});
+        return;
+    }
+    options.port = map.port;
+    options.host = map.host;
+    if ('username' in packet){
+        options.username = packet.username;
+        options.password = packet.password;
+    }
+    options.clientId = packet.clientId;
+    options.clean = packet.clientId;
+    options.keepalive = packet.keepalive;
+    options.dup = packet.dup;
+    options.qos = packet.qos;
+    options.protocolVersion = packet.protocolVersion;
+    options.protocolId = packet.protocolId;
+    self.proxies[client.id] = mqtt.connect(options);
+    self.clients[packet.clientId] = client;
+
+    var proxy = self.proxies[client.id]
+
+    proxy.on('connect',function(){
+        console.log('Proxy connect')
+        client.connack({returnCode:0});
+     });
+
+    proxy.on('connack',function(_packet){
+        console.log('Connack ' + packet)
+        client.connack({returnCode: _packet.returnCode});
+     });
+
+    proxy.on('suback',function(_packet){
+     console.log('proxy suback');
+     console.log(_packet);
+    	client.suback({messageId: _packet.messageId, granted: _packet.qos});
+	});
+    proxy.on('message',function(topic,payload,qos){
+   	console.log(topic);
+    var c = client;
+
+      for (var i = 0; i < c.subscriptions.length; i++) {
+        var s = c.subscriptions[i];
+        if (s.test(topic)) {
+        
+          c.publish({topic: topic, payload: payload});
+          break;
+        }
+      }
+	});
+
+ 
+   proxy.on('puback',function(_packet){
+    	client.puback(_packet) 
+	});
+
+    proxy.on('pubrel', function (_packet) {
+    	 client.pubrel(_packet);
+    });
+
+    proxy.on('pubrec', function (_packet) {
+      	client.pubrel(_packet);
+    });
+
+    proxy.on('suback',function(_packet){
+      client.suback({
+        messageId: _packet.messageId,
+        granted: _packet.subscriptions.map(function (e) {
+          return e.qos;
+        })
+      });
+    });
+    
+    proxy.on('disconnect',function(rc){
+      console.log('proxy disconnect');
+     });
+
+    proxy.on('close',function(e){
+      console.log('proxy close' + (e != 'undefined') ? e:"");
+      
+      client.connack({returnCode:1}); // Add error codes (convert to ints i.e. ECONNREFUSED )
+    	 client.stream.end();
+      delete self.clients[packet.clientId];
+     	delete self.clients[client.id];
+    })
+
+    proxy.on('error', function(e) {
+       	console.log(e);
+        client.error(e);
+    });
+
+
+    console.log('Proxy handlers established')
+   
+  });
+
+  client.on('subscribe', function(packet) {
+    var granted = [];
+
+    console.log("SUBSCRIBE(%s): %j", client.id, packet);
+
+    for (var i = 0; i < packet.subscriptions.length; i++) {
+      var qos = packet.subscriptions[i].qos
+        , topic = packet.subscriptions[i].topic
+        , reg = new RegExp(topic.replace(new RegExp('\/',"g"),'\\/').replace('\$','\\$').replace('+', '[^\/]+').replace('#', '.+','g') + '$');
+
+      granted.push(qos);
+      client.subscriptions.push(reg);
+      if (client.id in self.proxies)
+          self.proxies[client.id].subscribe(topic,qos);
+    }
+
+  });
+
+
+  client.on('publish', function(packet) {
+    console.log("PUBLISH(%s): %j", client.id, packet);
+    if (client.id in self.proxies)
+        self.proxies[client.id].publish(packet.topic, packet.payload,packet.qos)
+  });
+
+  client.on('pingreq', function(packet) {
+    console.log('PINGREQ(%s)', client.id);
+    if (client.id in self.proxies && self.proxies[client.id].connected)
+       client.pingresp();
+  });
+
+  client.on('disconnect', function(packet) {
+    console.log("Got disconnect")
+    if (client.id in self.proxies)
+        self.proxies[client.id].end();
+  });
+
+  client.on('close', function(packet) {
+    console.log("close event!")
+    if (client.id in self.proxies)
+        self.proxies[client.id].end();
+  });
+
+  client.on('error', function(e) {
+    client.stream.end();
+    console.log(e);
+  });
+}).listen(process.argv[2] || 1884);
+
